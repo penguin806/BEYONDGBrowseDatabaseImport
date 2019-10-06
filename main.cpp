b0f6e1bf284b160d19723f7a5ee0cdc110e6b7ac
Snow <polarpenguin@foxmail.com>
2019-10-07 02:50:02 +0800

    Test all pass, program renamed

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QTextCodec *codec = QTextCodec::codecForName("GBK");
     QTextCodec::setCodecForLocale(codec);
     const QString applicationName =
-            QString::fromLocal8Bit("高通量多组学序列数据可视化浏览器 - 数据库初始化");
+            QString::fromLocal8Bit("BEYONDGBrowse-DatabaseImport");
     QApplication app(argc, argv);
     QApplication::setApplicationName(applicationName);
     QApplication::setApplicationVersion("1.0.0");
